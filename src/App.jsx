76000cc68db5fe56022fdbc5d299f8694b677499
Akash <akasgj007@gmail.com>
2025-07-31 14:19:59 +0530

    hide admin button in admin page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
-import { Link, Route, HashRouter as Router, Routes } from "react-router-dom";
+import {
+  Link,
+  Route,
+  HashRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
 import AdminPage from "./pages/AdminPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 
 function App() {
+  const location = useLocation();
   return (
     <Router>
       <div className="container mt-3">
-        <Link to="/login" className="btn btn-outline-secondary mb-3">
-          Admin Panel
-        </Link>
+        {location.pathname !== "/admin" && (
+          <Link to="/login" className="btn btn-outline-secondary mb-3">
+            Admin Panel
+          </Link>
+        )}
 
         <Routes>
           <Route path="/" element={<HomePage />} />
