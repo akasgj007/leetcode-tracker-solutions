b441426e32554a0745616dd64c19cffb1d0a2c03
Akash <akasgj007@gmail.com>
2025-07-31 13:50:31 +0530

    Update: latest changes

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-   base: '/leetcode-tracker-solutions/', 
+   base: '/', 
   plugins: [react()],
 })
